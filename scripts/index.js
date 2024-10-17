e523d3c1645b56dfdf447848d80a196b9a5764f7
SERENA FERRARO <serenaferraro21@gmail.com>
2024-10-17 15:20:14 +0200

    Numbers

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,9 @@
-
-
-const selectedNumber = Math.ceil(Math.random() * 76);
 const tableTombola = document.getElementById("tombola");
+const selectNumberBtn = document.getElementById("selectNumberBtn");
 
 const createTable = () => {
- 
-
-
   for (let i = 1; i <= 76; i++) {
-   const cell = document.createElement("div");
+    const cell = document.createElement("div");
     cell.classList.add("number");
     cell.innerText = i;
 
@@ -16,4 +11,23 @@ const createTable = () => {
   }
 };
 
-createTable();
+const pickNumber = () => {
+  const selectedNumber = Math.ceil(Math.random() * 76);
+  const selectedDiv = document.getElementsByClassName("number");
+  
+  for (let i = 0; i < selectedDiv.length; i++) {
+    if (parseInt(selectedDiv[i].innerText)=== selectedNumber) {
+      selectedDiv[i].classList.add("numberDrawn");
+    }
+  }
+
+ 
+
+  console.log("pulsante premuto", selectedNumber);
+};
+
+
+
+createTable()
+selectNumberBtn.addEventListener("click", pickNumber);
+;
